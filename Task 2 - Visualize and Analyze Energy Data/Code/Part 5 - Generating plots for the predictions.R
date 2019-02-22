20f8ebaa544b0b073e24155f4617983b312b07ec
Jeroen Meij <jeroenmeij12@gmail.com>
2019-02-22 21:50:24 +0100

    Ubiqum objective 3: IoT Analytics

--- Task 2 - Visualize and Analyze Energy Data/Code/Part 5 - Generating plots for the predictions.R
+++ Task 2 - Visualize and Analyze Energy Data/Code/Part 5 - Generating plots for the predictions.R
@@ -30,46 +30,19 @@ ggplot(forecast_GAP) +
 
 
 
-#preprocess weekly trend values
-weekly_values <- data.frame(values = forecast_GAP$weekly[c(1:7)], 
-                            day = c("Monday",
-                                    "Tuesday",
-                                    "Wednesday",
-                                    "Thursday",
-                                    "Friday",
-                                    "Saturday",
-                                    "Sunday"))
-
-weekly_values$day <- as.character(weekly_values$day)
-weekly_values$day  <- factor(weekly_values$day, levels=unique(weekly_values$day))
-
-
-
-#plot weekly trend
-ggplot(weekly_values) +
-  geom_hline(yintercept = 0,
-             size = 1,
-             colour="#333333",
-             linetype="dashed") +
-  geom_path(aes(x = day, y = values),
-            colour = "#FAAB18",
-            size = 1.2,
-            group = 1) + 
-  geom_hline(yintercept = -.15, 
-             size = 1,
-             colour="#333333") +
-  bbc_style() +
-  labs(title="Weekly Trend",
-       subtitle = "Relative electricity usage per day")
-
-
-
 #preprocess yearly trend values
 year_vector_2007 <- c(seq.Date(from = date('2007-01-01'),
                                to = date('2007-12-31'),
                                by = "day"))
 Yearly_values <- data.frame(values = forecast_GAP$yearly[c(1:365)], 
                             day = year_vector_2007)
+
+
+Yearly_values$id = 0
+Yearly_values %>% filter(values < 0) %>% mutate(id = 1)
+
+
+
 
 
 #plot yearly trend
@@ -86,8 +59,51 @@ ggplot(Yearly_values) +
              size = 1,
              colour="#333333") +
   bbc_style() +
+  
+scale_y_continuous(breaks = c(-0.4, -0.2, 0, 0.2, 0.4),
+labels = c("-40%","-20%","Yearly
+Average
+kWh ",
+           "+20%", "+40%")) +
+  
+  theme(panel.grid.major.x = element_line(color="#cbcbcb"), 
+        panel.grid.major.y=element_blank()) +
+  
+  scale_x_date(date_breaks = "3 months",
+               date_labels = c("Fall", "Winter", "Spring",  "Summer")) +
+  
+  geom_curve(aes(x = as.Date("2007-10-26"), y = -0.4, xend = as.Date("2007-08-26"), yend = -.4), 
+             colour = "#555555", 
+             size=0.8,
+             curvature = -0.3,
+             arrow = arrow(length = unit(0.04, "npc")) ) +
+
+  geom_curve(aes(x = as.Date("2007-05-05"), y = 0.22, xend = as.Date("2007-02-20"), yend = .30), 
+             colour = "#555555", 
+             size=0.8,
+             curvature = 0.8,
+             arrow = arrow(length = unit(0.04, "npc")) ) +
+  
+    
+  geom_label(aes(x = as.Date("2007-10-12"), y = -.38, label = "During the summer months\nenergy consumption is\nat its lowest point"), 
+             hjust = 0, 
+             vjust = 0,
+             lineheight = 0.8,
+             colour = "#555555", 
+             fill = "white", 
+             label.size = NA, 
+             family="Helvetica", 
+             size = 6) +
+  
+  theme(axis.text.x = element_text(hjust = 3)) +
+  
+  
   labs(title="Yearly Trend",
-       subtitle = "Relative Electricity Usage In A Year")
+       subtitle = "Average electricity usage over the year of a given household") + 
+  theme(plot.subtitle=element_text(face="italic", color="red3"))
+
+
+
 
 
 
@@ -113,6 +129,30 @@ ggplot(forecast_GAP) +
   bbc_style() +
   labs(title="Prophet Forecast",
        subtitle = "Average kWh Per Day From 2007-2012")
+
+
+forecast_GAP$ds <- date(forecast_GAP$ds)
+
+#plot all trends
+ggplot(forecast_GAP) +
+  geom_point(aes(x = ds,
+                  y = y*60),
+              colour = "darkred",
+              size = 1,
+             alpha = 0.5) +
+  geom_hline(yintercept = 0,
+             size = 1,
+             colour="#333333") +
+  bbc_style() +
+  labs(title="Prophet Forecast",
+       subtitle = "Average kWh Per Day From 2007-2012")
+
+
+
+
+
+
+
 
 
 #plot all trends with confidence interval
---
+++ Task 2 - Visualize and Analyze Energy Data/Code/Part 5, Sub 1 - Plot weekly trend.R
@@ -0,0 +1,98 @@
+#####################################################
+# Date:      21-02-2019                             #
+# Author:    Jeroen Meij                            #
+# File:      Analysis of Smart Home electronic data #
+# Version:   1.0                                    #    
+#####################################################
+
+
+
+#plot Weekly trend
+###################################################
+
+#preprocess weekly trend values
+weekly_values <- data.frame(values = forecast_GAP$weekly[c(1:7)], 
+                            day = c("Monday",
+                                    "Tuesday",
+                                    "Wednesday",
+                                    "Thursday",
+                                    "Friday",
+                                    "Saturday",
+                                    "Sunday"))
+
+weekly_values$day <- as.character(weekly_values$day)
+weekly_values$day  <- factor(weekly_values$day, levels=unique(weekly_values$day))
+
+
+
+#plot weekly trend
+ggplot(weekly_values) +
+  geom_hline(yintercept = 0,
+             size = 1,
+             colour="darkred",
+             linetype="dashed") +
+  geom_path(aes(x = day,
+                y = (values)),
+            colour = "#FAAB18",
+            size = 1.2,
+            group = 1) +
+  #add text  
+  geom_label(aes(x = "Wednesday", y = .13, label = "On Saturday and Sunday,\nmost energy is consumed!"), 
+             hjust = 0, 
+             vjust = 0.5, 
+             lineheight = 0.8,
+             colour = "#555555", 
+             fill = "white", 
+             label.size = NA, 
+             family="Helvetica", 
+             size = 6) +
+  
+  
+  geom_label(aes(x = "Wednesday", y = .02, label = "Wednesday is\n  laundry day"), 
+             hjust = +0.39, 
+             vjust = 0.6,
+             lineheight = 0.8,
+             colour = "#555555", 
+             fill = "white", 
+             label.size = NA, 
+             family="Helvetica", 
+             size = 6) +
+  
+  geom_label(aes(x = "Monday", y = -.11, label = "On weekdays the family is working,\ncausing a lower energy consumption"), 
+             hjust = 0.1, 
+             vjust = 0.5,
+             lineheight = 0.8,
+             colour = "#555555", 
+             fill = "white", 
+             label.size = NA, 
+             family="Helvetica", 
+             size = 6) +
+  
+  
+  #add arrow  
+  geom_curve(aes(x = "Saturday", y = .12, xend = "Thursday", yend = .14), 
+             colour = "#555555", 
+             size=0.5, 
+             curvature = +0.3,
+             arrow = arrow(length = unit(0.03, "npc")) ) +
+  
+  
+  
+  geom_hline(yintercept = -.15, 
+             size = 1,
+             colour="#333333") +
+  
+  
+  bbc_style() +
+  
+  
+  scale_y_continuous(breaks = c(-0.1, 0, 0.1),
+                     labels = c("-10%","Average
+                                Weekly
+                                kWh 
+                                ", "+10%")) +
+  labs(title="Weekly Trend",
+       subtitle = "Average electricity usage per day of a French household") + 
+  theme(plot.subtitle=element_text(face="italic", color="red3"))
+
+
